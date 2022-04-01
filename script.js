4657b9e4b39e60df16bf1a10215de64d257112ee
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-04-01 21:26:44 +0100

    Fixed typo & added comments to JavaScript file

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-//grabbing each section of the website and the hidden paragraph, 'section_1_dropdown_content'
+//grabbing each section
 const section_1_hover_btn = document.getElementById('section_1_hover_btn');
 const section_2_hover_btn = document.getElementById('section_2_hover_btn');
 const section_3_hover_btn = document.getElementById('section_3_hover_btn');
 const section_4_hover_btn = document.getElementById('section_4_hover_btn');
 const section_5_hover_btn = document.getElementById('section_5_hover_btn');
 
+//grabbing the dropdown content of section 1
 const section_1_dropdown_content = document.getElementById('section_1_dropdown_content');
 
 //adding an event listener so that once the user presses the section...
 section_1_hover_btn.addEventListener("click", (e)=> {
 
-    //...we'll check whether or not the dropdown section has been already been activated or not (display:inline;)
+    //...we'll check whether or not the dropdown section has already been activated or not (display:inline;)
 
     //Whenever the dropdown section has been activated...
     if(section_1_dropdown_content.style.display == "inline"){
-        //...we'll deactivate the dropdown (display:none)
+        //...we'll deactivate the dropdown section (display:none)
         section_1_dropdown_content.style.display = "none";
 
-        //leave this event function
+        //leave this event function when finished.
         return;
     }
 
-    //Whenever the dropdown section has been activated...
+    //Whenever the dropdown section has been deactivated...
     else if(section_1_dropdown_content.style.display = "none"){
-        //...we'll deactivate the dropdown (display:none)
+        //...we'll activate the dropdown section (display:none)
         section_1_dropdown_content.style.display = "inline";
 
-        //leave this event function
+        //leave this event function when finished
         return;
     }
 });
